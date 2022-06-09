5d44f53ed378a4e223582f7ae82a7c7a487019e3
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-06-09 09:29:18 +0300

    Update this script to replace the value 12 with 89

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12,
+  name: 'Afande Ojok',
+  sex: 'Male',
+  company: 'Jok-Code AI',
+};
+
+console.log(myObject);
+myObject.value = 89;
+myObject.name = 'Ojok Simon Peter';
+myObject.company = 'Yearn AI';
+console.log(myObject);
